acc0f25d0505fa1ffac99d2d07d37de76f27ae78
ghoshpoulomi08 <ghoshpoulomi1308@gmail.com>
2023-08-25 11:53:42 +0530

    Implemented generate pdf file by using fpdf package

---
+++ main.py
@@ -0,0 +1,35 @@
+from fpdf import FPDF
+import pandas
+
+pdf = FPDF(orientation='P', unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin = 0)
+
+df = pandas.read_csv('topics.csv')
+
+for index, rows in df.iterrows():
+    pdf.add_page()
+    pdf.set_font('Arial', 'B', 16)
+    pdf.cell(40, 10, txt=rows["Topic"], ln=1)
+    pdf.line(x1=10, y1=22, x2=230, y2=22)
+
+    #Footer
+    pdf.ln(265)
+    pdf.set_font('Arial', 'I', 8)
+    pdf.cell(0, 10, txt=rows["Topic"], ln=0, align='R')
+
+
+    for i in range(rows["Pages"]):
+        pdf.add_page()
+        pdf.set_font('Arial', 'B', 16)
+        pdf.cell(40, 10, txt=rows["Topic"], ln=1)
+        pdf.line(x1=10, y1=22, x2=230, y2=22)
+
+        # Footer
+        pdf.ln(265)
+        pdf.set_font('Arial', 'I', 8)
+        pdf.cell(0, 10, txt=rows["Topic"], ln=0, align='R')
+
+pdf.output('new.pdf')
+
+
+
